9e9d63011dfd277909084f66e481ed4f475bba68
actbit <57023457+actbit@users.noreply.github.com>
2025-07-24 15:00:07 +0900

    プロジェクト ファイルを追加します。

---
+++ HideTerminal/Program.cs
@@ -0,0 +1,91 @@
+﻿using ConsoleAppFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HideTerminal
+{
+    class Program
+    {
+        public static SettingModel SettingModel { get; set; }
+        static async Task Main(string[] args)
+        {
+            if (File.Exists("setting.json"))
+            {
+                SettingModel = JsonSerializer.Deserialize<SettingModel>(System.IO.File.ReadAllText("setting.json"));
+            }
+            else
+            {
+                SettingModel = new SettingModel();
+                using (FileStream fs = new FileStream("setting.json", FileMode.OpenOrCreate))
+                {
+                    JsonSerializer.Serialize(fs, SettingModel);
+                    await fs.FlushAsync();
+                }
+            }
+            if (args.Length == 0)
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo(SettingModel.ExecutePath, SettingModel.Arguments);
+                processStartInfo.CreateNoWindow = true;
+                processStartInfo.WorkingDirectory = SettingModel.CurrentDirectory;
+                processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                try
+                {
+                    Process process = Process.Start(processStartInfo);
+
+                }
+                catch
+                {
+
+                }
+                return;
+            }
+            var consoleApp = ConsoleApp.Create();
+            consoleApp.Add<Set>("set");
+            consoleApp.Run(args);
+        }
+    }
+    class SettingModel
+    {
+        public string CurrentDirectory { get; set; } = "";
+        public string ExecutePath { get; set; } = "";
+        public string Arguments { get; set; } = "";
+    }
+    class Set
+    {
+        /// <summary>
+        /// Sets the current directory, executable path, and arguments for the application.
+        /// </summary>
+        /// <param name="current">-c, Current Directory</param>
+        /// <param name="execute">-e, Execute File</param>
+        /// <param name="args">-a, args</param>
+        [Command("")]
+        public void Run( string? current = null,string? execute = null, string? args = null)
+        {
+            if (!string.IsNullOrEmpty(current))
+            {
+                Program.SettingModel.CurrentDirectory = current.Trim('\"').Trim('\'');
+            }
+            if (!string.IsNullOrEmpty(execute))
+            {
+                Program.SettingModel.ExecutePath = execute.Trim('\"').Trim('\'');
+            }
+            if (!string.IsNullOrEmpty(args))
+            {
+                Program.SettingModel.Arguments = args.Trim('\"').Trim('\'');
+            }
+            using (FileStream fs = new FileStream("setting.json", FileMode.Open))
+            {
+                JsonSerializer.Serialize(fs, Program.SettingModel);
+            }
+        }
+    }
+
+}
